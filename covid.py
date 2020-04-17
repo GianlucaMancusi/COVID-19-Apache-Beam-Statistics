d6ebeff96b13884a70f9d8839fb916776ceed21c
Gianluca Mancusi <gianvyp@gmail.com>
2020-04-17 21:43:14 +0200

    English translation

--- covid.py
+++ covid.py
@@ -7,49 +7,51 @@ from datetime import datetime
 input_filename = "dpc-covid19-ita-province.csv"
 output_filename = "output.txt"
 
-# cose da fare:
-# - Opzioni per cambiare parametri
-# - Sistemare l'output
-
 
 class NewOptions(PipelineOptions):
-  @classmethod
-  def _add_argparse_args(cls, parser):
-    parser.add_argument('--input', help='Input for the pipeline')
-    parser.add_argument('--output', help='Output for the pipeline')
-    parser.add_argument('--ntop', help='Number of top day cases to show', default='3')
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument('--input', help='Input for the pipeline')
+        parser.add_argument('--output', help='Output for the pipeline')
+        parser.add_argument(
+            '--ntop', help='Number of top day cases to show', default='3')
+
 
 class Split(beam.DoFn):
     def process(self, element):
         data, __, ___, denominazione_regione, \
             ____, denominazione_provincia, \
-            _____, ______, _______, totale_casi, \
+            _____, ______, _______, total_cases, \
             ________, _________ = element.split(",")
-        return [{'regione': denominazione_regione,
-                 'provincia': denominazione_provincia,
-                 'data': data,
-                 'totale_casi': totale_casi}]
+        return [{'region': denominazione_regione,
+                 'province': denominazione_provincia,
+                 'date': data,
+                 'total_cases': total_cases}]
 
 
-class DeleteDatiInAggiornamento(beam.DoFn):
+class DeleteDataNotUpToDate(beam.DoFn):
     def process(self, element):
-        return None if 'In fase di definizione/aggiornamento' in element["provincia"] else [element]
+        return None if 'In fase di definizione/aggiornamento' in element["province"] else [element]
 
 
-class CollectRegioneProvincia(beam.DoFn):
+class CollectLocationKey(beam.DoFn):
     def process(self, element):
-        return [(f"{element['regione']},{element['provincia']}", int(element['totale_casi']))]
+        return [(f"{element['region']},{element['province']}", int(element['total_cases']))]
 
 
-class CollectRegioneProvinciaConData(beam.DoFn):
+class CollectLocationKeyWithDate(beam.DoFn):
     def process(self, element):
-        data_casi = (parser.parse(element['data']), int(
-            element['totale_casi']))
-        regione_provincia = f"{element['regione']},{element['provincia']}"
-        return [(regione_provincia, data_casi)]
+        cases_dates = (parser.parse(element['date']), int(
+            element['total_cases']))
+        region_province = f"{element['regione']},{element['province']}"
+        return [(region_province, cases_dates)]
 
 
 class Stddev(beam.CombineFn):
+    '''
+    This CombineFn type function calculates mean,
+    variance and standard deviation of the COVID-19 cases.
+    '''
     def create_accumulator(self):
         return (0.0, 0.0, 0)  # x, x^2, count
 
@@ -69,70 +71,71 @@ class Stddev(beam.CombineFn):
             variance = (sumsq / count) - mean*mean  # E(x^2) - E(x)*E(x)
             stddev = np.sqrt(variance) if variance > 0 else 0
         return {
-            'media': mean if count else float('NaN'),
-            'varianza': variance if count else float('NaN'),
+            'mean': mean if count else float('NaN'),
+            'variance': variance if count else float('NaN'),
             'stddev': stddev if count else float('NaN')
         }
 
 
 class GetLastDatesOnly(beam.CombineFn):
+    '''
+    This CombineFn type function calculates the maximum date 
+    and returns only that with the value of COVID-19 cases.
+    '''
     def create_accumulator(self):
-        return (datetime.min, 0.0)  # data, casi
+        return (datetime.min, 0.0)  # dates, cases
 
-    def add_input(self, data_casi, input):
-        (data_max, casi_data_max) = data_casi
-        (data_input, casi_input) = input
-        # prende solo la data più recente con il suo numero di casi
-        return (data_max, casi_data_max) if data_max > data_input else (data_input, casi_input)
+    def add_input(self, dates_cases, input):
+        (max_date, cases_max_date) = dates_cases
+        (input_date, input_cases) = input
+        # only takes the most recent date with its number of cases
+        return (max_date, cases_max_date) if max_date > input_date else (input_date, input_cases)
 
     def merge_accumulators(self, accumulators):
-        (date, casi_date) = zip(*accumulators)
-        # prende l'indice della data più recente
-        index = np.argmax(date)
-        return (date[index], casi_date[index])
+        (dates, cases) = zip(*accumulators)
+        # takes the index of the most recent date
+        index = np.argmax(dates)
+        return (dates[index], cases[index])
 
-    def extract_output(self, data_casi):
-        (data, casi) = data_casi
-        return {'data': "{:%Y-%m-%d}".format(data), 'casi': casi}
+    def extract_output(self, dates_cases):
+        (date, cases) = dates_cases
+        return {'date': "{:%Y-%m-%d}".format(date), 'cases': cases}
+
 
 new_options = NewOptions()
 with beam.Pipeline(options=new_options) as p:
     csv_formatted_data = (p
-                          | beam.io.ReadFromText(new_options.input, skip_header_lines=1)
-                          | beam.ParDo(Split())
-                          | beam.ParDo(DeleteDatiInAggiornamento())
+                          | "Reading the input dataset" >> beam.io.ReadFromText(new_options.input, skip_header_lines=1)
+                          | "Splitting the data row by row" >> beam.ParDo(Split())
+                          | "Deleting data not up to date" >> beam.ParDo(DeleteDataNotUpToDate())
                           )
 
-    grouped_by_regione_provincia = (csv_formatted_data
-                                    | beam.ParDo(CollectRegioneProvincia())
-                                    | "Grouping by regione_provincia" >> beam.GroupByKey()
+    grouped_by_location = (csv_formatted_data
+                                    | "Colleting Location as Key" >> beam.ParDo(CollectLocationKey())
+                                    | "Grouping by location" >> beam.GroupByKey()
                                     )
-    grouped_by_regione_provincia_con_data = (csv_formatted_data
-                                             | beam.ParDo(CollectRegioneProvinciaConData())
-                                             | "Grouping by regione_provincia_con_data" >> beam.GroupByKey()
+    grouped_by_location_and_date = (csv_formatted_data
+                                             | "Colleting Location and Date as Key" >> beam.ParDo(CollectLocationKeyWithDate())
+                                             | "Grouping by location with date" >> beam.GroupByKey()
                                              )
 
-    # mean_cases = (grouped_by_regione_provincia
-    #              | "Calculating mean" >> beam.CombineValues(beam.combiners.MeanCombineFn())
-    #              )
-
-    variance_cases = (grouped_by_regione_provincia
+    variance_cases = (grouped_by_location
                       | "Calculating variance and stddev" >> beam.CombineValues(Stddev())
                       )
 
-    top_cases = (grouped_by_regione_provincia
+    top_cases = (grouped_by_location
                  | "Calculating top 3" >> beam.CombineValues(beam.combiners.TopCombineFn(n=int(new_options.ntop)))
                  )
 
-    last_dates = (grouped_by_regione_provincia_con_data
+    last_dates = (grouped_by_location_and_date
                   | beam.CombineValues(GetLastDatesOnly())
                   )
 
     output_pipe = (
         {
-            'statistica_casi': variance_cases,
-            f'top_{new_options.ntop}_casi': top_cases,
-            'ultimi_dati': last_dates
+            'cases_statistics': variance_cases,
+            f'top_{new_options.ntop}_cases': top_cases,
+            'last_data': last_dates
         }
         | beam.CoGroupByKey()
         | beam.io.WriteToText(new_options.output))
