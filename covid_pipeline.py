8bb5cdff5a5f9cdf7aca5993b0c019091838bae8
Gianluca Mancusi <gianvyp@gmail.com>
2020-04-18 15:01:27 +0200

    Now it works on GCP

--- covid_pipeline.py
+++ covid_pipeline.py
@@ -2,13 +2,111 @@
 # Copyright (c) 2020 Gianluca Mancusi
 
 import apache_beam as beam
-import numpy as np
+
 import json
 
-# All the data extraction and data processing logic is present in these files
-from components import covid_options
-from components import par_do as CovidParDo
-from components import combines as CovidCombines
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+
+class CovidOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument('--input', help='Input dataset for the pipeline')
+        parser.add_argument(
+            '--output', help='Output JSON file name for the pipeline')
+        parser.add_argument(
+            '--ntop', type=int, help='Number of top day cases to show', default='3')
+
+
+class Split(beam.DoFn):
+    def process(self, element):
+        data, __, ___, denominazione_regione, \
+            ____, denominazione_provincia, \
+            _____, ______, _______, total_cases, \
+            ________, _________ = element.split(",")
+        return [{'region': denominazione_regione,
+                 'province': denominazione_provincia,
+                 'date': data,
+                 'total_cases': total_cases}]
+
+
+class DeleteDataNotUpToDate(beam.DoFn):
+    def process(self, element):
+        return None if 'In fase di definizione/aggiornamento' in element["province"] else [element]
+
+
+class CollectLocationKey(beam.DoFn):
+    def process(self, element):
+        return [(f"{element['region']},{element['province']}", int(element['total_cases']))]
+
+
+class CollectLocationKeyWithDate(beam.DoFn):
+    def process(self, element):
+        from dateutil import parser
+        cases_dates = (parser.parse(element['date']), int(
+            element['total_cases']))
+        region_province = f"{element['region']},{element['province']}"
+        return [(region_province, cases_dates)]
+
+
+class MeanVarStddev(beam.CombineFn):
+    '''
+    This CombineFn type function calculates mean,
+    variance and standard deviation of the COVID-19 cases.
+    '''
+
+    def create_accumulator(self):
+        return (0.0, 0.0, 0)  # x, x^2, count
+
+    def add_input(self, sum_count, input):
+        (sum, sumsq, count) = sum_count
+        return sum + input, sumsq + input*input, count + 1
+
+    def merge_accumulators(self, accumulators):
+        sums, sumsqs, counts = zip(*accumulators)
+        return sum(sums), sum(sumsqs), sum(counts)
+
+    def extract_output(self, sum_count):
+        import numpy as np
+        (sum, sumsq, count) = sum_count
+        mean, variance, stddev = 0, 0, 0
+        if count:
+            mean = sum / count
+            variance = (sumsq / count) - mean*mean  # E(x^2) - E(x)*E(x)
+            stddev = np.sqrt(variance) if variance > 0 else 0
+        return {
+            'mean': mean if count else float('NaN'),
+            'variance': variance if count else float('NaN'),
+            'stddev': stddev if count else float('NaN')
+        }
+
+
+class GetLastDatesOnly(beam.CombineFn):
+    '''
+    This CombineFn type function calculates the maximum date 
+    and returns only that with the value of COVID-19 cases.
+    '''
+    def create_accumulator(self):
+        from datetime import datetime
+        return (datetime.min, 0.0)  # dates, cases
+
+    def add_input(self, dates_cases, input):
+        (max_date, cases_max_date) = dates_cases
+        (input_date, input_cases) = input
+        # only takes the most recent date with its number of cases
+        return (max_date, cases_max_date) if max_date > input_date else (input_date, input_cases)
+
+    def merge_accumulators(self, accumulators):
+        import numpy as np
+        (dates, cases) = zip(*accumulators)
+        # takes the index of the most recent date
+        index = np.argmax(dates)
+        return (dates[index], cases[index])
+
+    def extract_output(self, dates_cases):
+        (date, cases) = dates_cases
+        return {'date': "{:%Y-%m-%d}".format(date), 'cases': cases}
 
 
 def covid_pipeline():
@@ -19,25 +117,25 @@ def covid_pipeline():
 
     see more: https://github.com/GianlucaMancusi/COVID-19-Apache-Beam-Statistics
     """
-    options = covid_options.CovidOptions()
+    options = CovidOptions()
     with beam.Pipeline(options=options) as p:
         csv_formatted_data = (p
                               | "Reading the input dataset" >> beam.io.ReadFromText(options.input, skip_header_lines=1)
-                              | "Splitting the data row by row" >> beam.ParDo(CovidParDo.Split())
-                              | "Deleting data not up to date" >> beam.ParDo(CovidParDo.DeleteDataNotUpToDate())
+                              | "Splitting the data row by row" >> beam.ParDo(Split())
+                              | "Deleting data not up to date" >> beam.ParDo(DeleteDataNotUpToDate())
                               )
 
         grouped_by_location = (csv_formatted_data
-                               | "Colleting Location as Key" >> beam.ParDo(CovidParDo.CollectLocationKey())
+                               | "Colleting Location as Key" >> beam.ParDo(CollectLocationKey())
                                | "Grouping by location" >> beam.GroupByKey()
                                )
         grouped_by_location_and_date = (csv_formatted_data
-                                        | "Colleting Location and Date as Key" >> beam.ParDo(CovidParDo.CollectLocationKeyWithDate())
+                                        | "Colleting Location and Date as Key" >> beam.ParDo(CollectLocationKeyWithDate())
                                         | "Grouping by location with date" >> beam.GroupByKey()
                                         )
 
         variance_cases = (grouped_by_location
-                          | "Calculating variance and stddev" >> beam.CombineValues(CovidCombines.MeanVarStddev())
+                          | "Calculating variance and stddev" >> beam.CombineValues(MeanVarStddev())
                           )
 
         top_cases = (grouped_by_location
@@ -45,7 +143,7 @@ def covid_pipeline():
                      )
 
         last_dates = (grouped_by_location_and_date
-                      | "Calculating last date and last cases" >> beam.CombineValues(CovidCombines.GetLastDatesOnly())
+                      | "Calculating last date and last cases" >> beam.CombineValues(GetLastDatesOnly())
                       )
 
         output_pipe = (
